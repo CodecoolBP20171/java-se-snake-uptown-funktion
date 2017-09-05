04c2a6e18e631ff3d5adeddc34af6137e86cb1c2
Bela-Szilagyi <szilagyib.mail@gmail.com>
2017-09-05 10:36:15 +0200

    Update Snake.java

--- src/com/codecool/snake/Snake.java
+++ src/com/codecool/snake/Snake.java
@@ -6,7 +6,6 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.util.PrimitiveIterator;
