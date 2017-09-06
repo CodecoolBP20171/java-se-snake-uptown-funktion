3d8709a699ecb7c5f4bbe38691346bf3e2bd83ca
peterosz <itharius@gmail.com>
2017-09-06 14:03:03 +0200

    deleted dead code

--- src/com/codecool/snake/entities/enemies/SimpleEnemy.java
+++ src/com/codecool/snake/entities/enemies/SimpleEnemy.java
@@ -1,13 +1,10 @@
 package com.codecool.snake.entities.enemies;
 
 import com.codecool.snake.Globals;
-import com.codecool.snake.Utils;
 import com.codecool.snake.entities.snakes.SnakeHead;
 import javafx.scene.layout.Pane;
-import java.util.Random;
 
 
-// a simple enemy TODO make better ones.
 public class SimpleEnemy extends Enemy {
 
     public SimpleEnemy(Pane pane, SnakeHead snake) {
@@ -17,13 +14,6 @@ public class SimpleEnemy extends Enemy {
         pane.getChildren().add(this);
 
         speed = 1;
-        Random rnd = new Random();
-        setX(rnd.nextDouble() * Globals.WINDOW_WIDTH);
-        setY(rnd.nextDouble() * Globals.WINDOW_HEIGHT);
-
-        direction = rnd.nextDouble() * 360;
-        setRotate(direction);
-        heading = Utils.directionToVector(direction, speed);
 
         this.id = ++numberOfSimpleEnemies;
 
